8fd8ed1f2292b29b889d593a51c6fc77ef0d395e
S-ARVIND01 <arvindskating1@gmail.com>
2022-12-12 19:15:57 +0530

    helllo

--- py.py
+++ py.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to display the top 5 programming language.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content = """
@@ -43,15 +25,3 @@ server_address = ('', 8080)
 httpd = HTTPServer(server_address, HelloHandler)
 print("my webserver is running.....")
 httpd.serve_forever()
-```
-
-
-## OUTPUT:
-![output](pic1.png)
-
-![output](pic2.png)
-
-
-## RESULT:
-
-Thus this program diaplay top 5 programming language.
